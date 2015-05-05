2334a8c6ff1c686b817c8e252aa739532d5769bd
Furacin <manzanile@gmail.com>
2015-05-05 17:00:45 +0200

    Eliminada carpeta innecesaria

--- web/web/comollegar.php
+++
@@ -1,8 +0,0 @@
-<h1><img class="logo" src="images/ic_action_map.png"> Cómo llegar</h1>  
-
-<?php
-if($_GET['site'] == "etsiit")
-    include "etsiit.php";
-else
-    include "granada.php";
-?>
--- web/web/sidebar.php
+++
@@ -1,7 +0,0 @@
-<h1>Relevante</h1>
-<ul class="menu-sidebar">
-    <li><a href="?secc=inscripcion">Inscripción</a></li>
-    <li><a href="?secc=programa">Ponencias</a></li>
-    <li><a href="?secc=patrocinadores">Cómo patrocinar</a></li>
-    <li id="ultimo"><a href="?secc=contacto">Contacto</a></li>
-</ul>
